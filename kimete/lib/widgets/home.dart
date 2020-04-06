d605051116a6fc2929e7afc53a8bfcc67b8a810c
Takanori Fukuyama <takanori.fukuyama@i-kasa.com>
2020-04-06 10:17:22 +0900

    modified

--- kimete/lib/widgets/home.dart
+++ kimete/lib/widgets/home.dart
@@ -1,52 +1,56 @@
 import 'package:flutter/material.dart';
-import 'package:kimete/widgets/cards_section_alignment.dart';
+import 'package:flutter_tindercard/flutter_tindercard.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
+  @override
+  _ExampleHomePageState createState() => _ExampleHomePageState();
+}
+
+class _ExampleHomePageState extends State<Home>
+    with TickerProviderStateMixin {
+  List<String> welcomeImages = [
+    "https://raw.githubusercontent.com/ShaunRain/flutter_tindercard/master/assets/welcome0.png",
+    "https://raw.githubusercontent.com/ShaunRain/flutter_tindercard/master/assets/welcome1.png",
+    "https://raw.githubusercontent.com/ShaunRain/flutter_tindercard/master/assets/welcome2.png",
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: <Widget>[
-          CardsSectionAlignment(context),
-          buttonRow()
-        ],
+    CardController controller; //Use this to trigger swap.
+
+    return new Scaffold(
+      body: new Center(
+          child: Container(
+              height: MediaQuery.of(context).size.height * 0.6,
+              child: new TinderSwapCard(
+                  orientation: AmassOrientation.BOTTOM,
+                  totalNum: welcomeImages.length,
+                  stackNum: 3,
+                  swipeEdge: 4.0,
+                  maxWidth: MediaQuery.of(context).size.width * 0.9,
+                  maxHeight: MediaQuery.of(context).size.width * 0.9,
+                  minWidth: MediaQuery.of(context).size.width * 0.8,
+                  minHeight: MediaQuery.of(context).size.width * 0.8,
+                  cardBuilder: (context, index) => Card(
+                        child: Image.network('${welcomeImages[index]}'),
+                      ),
+                  cardController: controller = CardController(),
+                  swipeUpdateCallback:
+                      (DragUpdateDetails details, Alignment align) {
+                    /// Get swiping card's alignment
+                    if (align.x < 0) {
+                      //Card is LEFT swiping
+                    } else if (align.x > 0) {
+                      //Card is RIGHT swiping
+                    }
+                  },
+                  swipeCompleteCallback:
+                      (CardSwipeOrientation orientation, int index) {
+                    /// Get orientation & index of swiped card!
+                  },
+              ),
+          ),
       ),
-    );
-  }
-  Widget buttonRow() {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 48.0),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          FloatingActionButton(
-            mini: true,
-            onPressed: (){},
-            backgroundColor: Colors.white,
-            child: Icon(Icons.loop, color: Colors.yellow),
-          ),
-          Padding(padding: EdgeInsets.only(right: 8.0)),
-          FloatingActionButton(
-            onPressed: (){},
-            backgroundColor: Colors.white,
-            child: Icon(Icons.thumb_down, color: Colors.red),
-          ),
-          Padding(padding: EdgeInsets.only(right: 8.0)),
-          FloatingActionButton(
-            onPressed: () {},
-            backgroundColor: Colors.white,
-            child: Icon(Icons.thumb_up, color: Colors.green),
-          ),
-          Padding(padding: EdgeInsets.only(right: 8.0)),
-          FloatingActionButton(
-            mini: true,
-            onPressed: () {},
-            backgroundColor: Colors.white,
-            child: Icon(Icons.star, color: Colors.blue),
-          ),
-        ],
-      )
     );
   }
 }
